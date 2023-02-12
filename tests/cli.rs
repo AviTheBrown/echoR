abe2de399ca06247bdb70bbe6c2a4a79977601ac
Avi Brown <avinbrown.focus@icloud.com>
2023-02-12 00:02:11 -0500

    added the new type (typeResult) and corresponding tests.

--- tests/cli.rs
+++ tests/cli.rs
@@ -12,26 +12,28 @@ fn dies_no_args() {
         .stderr(predicate::str::contains("USAGE"));
 }
 
-#[test]
-fn hello1() -> TestResult {
-    let expected =
-    fs::read_to_string("tests/expected/hello.txt")?;
-    let mut cmd = Command::cargo_bin("echoR").unwrap();
-    cmd.arg("Hello there")
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
         .assert()
         .success()
         .stdout(expected);
-    OK(())
-}
+    Ok(()) }
 
 #[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+#[test]
 fn hello2() -> TestResult {
-    let expected =
-        fs::read_to_string("tests/expected/hello2.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(vec!["Hello", "there"])
-        .assert()
-        .success()
-        .stdout(expected);
-    OK(())
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
